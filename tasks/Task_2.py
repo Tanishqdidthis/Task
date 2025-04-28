e5d0023e44ab6823084ed899ca8556415fd29ef9
Tanishqdidthis <101451613+Tanishqdidthis@users.noreply.github.com>
2025-04-28 17:24:06 +0530

    Delete tasks/Task_2.py

--- tasks/Task_2.py
+++
@@ -1,6 +0,0 @@
-#As the sheet said to sum up only integers from 1 to 50 therefore no user input was taken
-
-count = 0
-for i in range(1, 51):
-    count += i
-print("The sum of numbers from 1 to 50 is: ",count)
